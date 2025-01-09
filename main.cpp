3fb369b50d869eacd050b7404827f1a332b6fde0
James Skön <skonjp@kenyon.edu>
2025-01-09 18:09:00 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // enter 6 integers into a size array
+    int sizes[6];
+    int h1, h2;
+
+    for (int i = 0; i < 6; ++i) {
+        cin >> size[i];
+    }
+    cin >> h1;
+    cin >> h2;    
+    
+
+    return 0;
+}
